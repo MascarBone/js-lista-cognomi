d7ce3b17f1c7c1e0ae5b8806f0175c89f8e77cba
MascarBone <simone.bonelli@gmail.com>
2021-08-26 16:17:58 +0200

    Creating input to save surname in the array

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
 let listaCognomi = [];
 let input;
+let i = 0;
+
+do{
+    input = prompt ("Inserisci un cognome");
+    if (input != 0) {
+        listaCognomi.push(input);
+    }
+
+}while (input != 0)
+
+console.log (listaCognomi);
